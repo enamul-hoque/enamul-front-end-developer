6ad1462504ce74b8dadc688cb3c1b67ef2f10e6c
Enamul Hoque Mohon <enamul.hoque.mohon@gmail.com>
2023-01-08 19:30:51 +0600

    Initial Setup.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <header className='header--section border-b'>
+        <div className='container py-8'>
+          <h1 className='header--logo text-3xl font-bold'>SpaceX</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
